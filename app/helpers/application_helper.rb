e286210288cecd4ede7dd3ed3032e7d6588e791d
Richard <richard.be.jamin@gmail.com>
2014-06-08 22:03:43 -0500

    added animate.css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
 	def flash_class(level)
 		case level
-			when :notice then "alert alert-info"
-			when :success then "alert alert-success"
-			when :error then "alert alert-danger"
-			when :warning then "alert alert-warning"
-			else "alert alert-warning"
+			when :notice then "alert alert-info animated fadeInDown"
+			when :success then "alert alert-success animated fadeInDown"
+			when :error then "alert alert-danger animated fadeInDown"
+			when :warning then "alert alert-warning animated fadeInDown"
+			else "alert alert-warning animated fadeInDown"
 		end
 	end
 
